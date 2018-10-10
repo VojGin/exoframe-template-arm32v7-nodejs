281c9266dc971163c8d6c7e9fe1f63522fb9df21
Vojtěch Gintner <gintnervojtech@gmail.com>
2018-10-10 14:53:46 +0200

    init

---
+++ index.js
@@ -0,0 +1,77 @@
+// npm packages
+const fs = require('fs');
+const path = require('path');
+
+const nodeDockerfile = ({hasYarn}) =>
+  `FROM yobasystems/alpine-nodejs:arm32v7-min
+
+# create folder and set it as workdir
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# copy package and yarn files to cache deps install
+COPY package.json /usr/src/app/${
+    hasYarn
+      ? `
+COPY yarn.lock /usr/src/app/
+RUN yarn install --production`
+      : `
+RUN npm install --production`
+  }
+
+# copy app itself
+COPY . /usr/src/app
+
+EXPOSE 80
+EXPOSE 443
+
+CMD ["npm", "start"]
+`;
+
+// template name
+exports.name = 'node';
+
+// function to check if the template fits this recipe
+exports.checkTemplate = async ({tempDockerDir}) => {
+  // if project already has dockerfile - just exit
+  try {
+    const filesList = fs.readdirSync(tempDockerDir);
+    if (filesList.includes('package.json')) {
+      return true;
+    }
+    return false;
+  } catch (e) {
+    return false;
+  }
+};
+
+// function to execute current template
+exports.executeTemplate = async ({username, tempDockerDir, resultStream, util, docker, existing}) => {
+  try {
+    // generate dockerfile
+    const filesList = fs.readdirSync(tempDockerDir);
+    const dockerfile = nodeDockerfile({hasYarn: filesList.includes('yarn.lock')});
+    const dfPath = path.join(tempDockerDir, 'Dockerfile');
+    fs.writeFileSync(dfPath, dockerfile, 'utf-8');
+    util.writeStatus(resultStream, {message: 'Deploying Node.js project..', level: 'info'});
+
+    // build docker image
+    const buildRes = await docker.build({username, resultStream});
+    util.logger.debug('Build result:', buildRes);
+
+    // start image
+    const container = await docker.start(Object.assign({}, buildRes, {username, existing, resultStream}));
+    util.logger.debug(container.Name);
+
+    // clean temp folder
+    await util.cleanTemp();
+
+    // return new deployments
+    util.writeStatus(resultStream, {message: 'Deployment success!', deployments: [container], level: 'info'});
+    resultStream.end('');
+  } catch (e) {
+    util.logger.debug('build failed!', e);
+    util.writeStatus(resultStream, {message: e.error, error: e.error, log: e.log, level: 'error'});
+    resultStream.end('');
+  }
+};
